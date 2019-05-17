f255a983ac6a40fb3ee81372bf333ee105f28878
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-17 02:13:28 +0000

    Snapshot of branch master taken on 2019-05-16T22:13:27.165-04:00 from "https://github.com/UnofficialJuliaMirror/DirectSum.jl-22fd7b30-a8c0-5bf2-aabe-97783860d07c"

--- src/indices.jl
+++ src/indices.jl
@@ -61,8 +61,39 @@ const VTI = Union{Vector{Int},Tuple,NTuple}
 end
 
 # index sets
+index_limit = 20
+const ndigits_cache = Vector{SVector}[]
+const ndigits_extra = Dict{Bits,SVector}[]
+@pure ndigits_calc(b,N) = SVector{N+1,Int}(digits(b,base=2,pad=N+1))
+@pure function ndigits(b,N)
+    if N>index_limit
+        n = N-index_limit
+        for k ∈ length(ndigits_extra)+1:n
+            push!(ndigits_extra,Dict{Bits,SVector{k+1,Int}}())
+        end
+        !haskey(ndigits_extra[n],b) && push!(ndigits_extra[n],b=>ndigits_calc(b,N))
+        @inbounds ndigits_extra[n][b]
+    else
+        for k ∈ length(ndigits_cache)+1:min(N,index_limit)
+            push!(ndigits_cache,[ndigits_calc(d,k) for d ∈ 0:1<<(k+1)-1])
+            GC.gc()
+        end
+        @inbounds ndigits_cache[N][b+1]
+    end
+end
+
 @pure indices(b::Bits) = findall(digits(b,base=2).==1)
-@pure shift_indices(V::T,b::Bits) where T<:VectorSpace = shift_indices(V,indices(b))
+@pure function indices(b::Bits,N::Int)
+    d = ndigits(b,N)
+    l = length(d)
+    a = Int[]
+    for i ∈ 1:l
+        d[i] == 1 && push!(a,i)
+    end
+    return a
+end
+
+@pure shift_indices(V::T,b::Bits) where T<:VectorSpace = shift_indices(V,indices(b,ndims(V)))
 function shift_indices(s::T,set::Vector{Int}) where T<:VectorSpace{N,M} where N where M
     if !isempty(set)
         k = 1
@@ -87,21 +118,73 @@ end
     C && printindices(io,c,l,g)
     D && printindices(io,d,l,h)
 end
-@pure function printindices(io::IO,V::T,e::Bits,label::Bool=false) where T<:VectorSpace
+@pure printindices(io::IO,V::T,e::Bits,label::Bool=false) where T<:VectorSpace = printlabel(io,V,e,label,pre...)
+
+@inline function printlabel(io::IO,V::T,e::Bits,label::Bool,vec,cov,duo,dif) where T<:VectorSpace
     N,D,C,db = ndims(V),diffmode(V),dualtype(V),dualbits(V)
     if C < 0
         es = e & (~(db[1]|db[2]))
         n = Int((N-2D)/2)
         eps = shift_indices(V,e & db[1]).-(N-2D)
         par = shift_indices(V,e & db[2]).-(N-D)
-        printindices(io,shift_indices(V,es & Bits(2^n-1)),shift_indices(V,es>>n),eps,par,label)
+        printindices(io,shift_indices(V,es & Bits(2^n-1)),shift_indices(V,es>>n),eps,par,label,vec,cov,duo,dif)
     else
         es = e & (~db)
         eps = shift_indices(V,e & db).-(N-D)
         if !isempty(eps)
-            printindices(io,shift_indices(V,es),Int[],C>0 ? Int[] : eps,C>0 ? eps : Int[],label,C>0 ? pre[2] : pre[1])
+            printindices(io,shift_indices(V,es),Int[],C>0 ? Int[] : eps,C>0 ? eps : Int[],label,C>0 ? cov : vec,cov,C>0 ? dif : duo,dif)
         else
-            printindices(io,shift_indices(V,es),label,C>0 ? pre[2] : pre[1])
+            printindices(io,shift_indices(V,es),label,C>0 ? string(cov) : vec)
         end
+    end
+end
+
+function indexparity!(ind::Vector{Int},s::VectorSpace{N,M} where N) where M
+    k = 1
+    t = false
+    while k < length(ind)
+        if ind[k] == ind[k+1]
+            ind[k] == 1 && hasinf(s) && (return t, ind, true)
+            s[ind[k]] && (t = !t)
+            deleteat!(ind,[k,k+1])
+        elseif ind[k] > ind[k+1]
+            ind[k:k+1] = ind[k+1:-1:k]
+            t = !t
+            k ≠ 1 && (k -= 1)
+        else
+            k += 1
+        end
+    end
+    return t, ind, false
+end
+
+@noinline function indexparity(V::VectorSpace,v::Symbol)::Tuple{Bool,Vector,VectorSpace,Bool}
+    vs = string(v)
+    vt = vs[1:1]≠pre[1]
+    Z=match(Regex("([$(pre[1])]([0-9a-vx-zA-VX-Z]+))?([$(pre[2])]([0-9a-zA-Z]+))?"),vs)
+    ef = String[]
+    for k ∈ (2,4)
+        Z[k] ≠ nothing && push!(ef,Z[k])
+    end
+    length(ef) == 0 && (return false,Int[],V,true)
+    let W = V,fs=false
+        C = dualtype(V)
+        X = C≥0 && ndims(V)<4sizeof(Bits)+1
+        X && (W = C>0 ? V'⊕V : V⊕V')
+        V2 = (vt ⊻ (vt ? C≠0 : C>0)) ? V' : V
+        L = length(ef) > 1
+        M = X ? Int(ndims(W)/2) : ndims(W)
+        m = ((!L) && vt && (C<0)) ? M : 0
+        chars = (L || (Z[2] ≠ nothing)) ? alphanumv : alphanumw
+        (es,e,et) = indexparity!([findfirst(isequal(ef[1][k]),chars) for k∈1:length(ef[1])].+m,C<0 ? V : V2)
+        et && (return false,Int[],V,true)
+        w,d = if L
+            (fs,f,ft) = indexparity!([findfirst(isequal(ef[2][k]),alphanumw) for k∈1:length(ef[2])].+M,W)
+            ft && (return false,Int[],V,true)
+            W,[e;f]
+        else
+            V2,e
+        end
+        return es⊻fs, d, w, false
     end
 end
